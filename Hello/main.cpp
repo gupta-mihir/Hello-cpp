40a28d4b2cfdd11235864447ecd5df2e11816afd
Mihir Gupta <guptamihir@protonmail.com>
2021-12-27 15:48:00 -0500

    Changed /n to endl, included string and namespace std because that's what I'm used to, got rid of individual std scope operators.

--- Hello/main.cpp
+++ Hello/main.cpp
@@ -4,10 +4,13 @@
 //  Created by Mihir Gupta on 12/27/21.
 
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
     
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!" << endl;
     
     return 0;
 }
